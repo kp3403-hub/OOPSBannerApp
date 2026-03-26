df8d066ce51002ac194cdcda3d68239c2494e182
kp3403-hub <kp3403@srmist.edu.in>
2026-03-26 13:58:49 +0530

    Printed-OOPS-Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,25 @@
+/**
+ * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
+ *
+ * This class extends the functionality of Use Case 2 by improving the way
+ * the "OOPS" banner is constructed and displayed. It uses the String.join()
+ * method to build each line of the banner instead of string concatenation.
+ *
+ * @author Developer
+ * @version 3.0
+ */
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" ***     ***    ******    ***** ");
-        System.out.println("** **   ** **   **   **  **   **");
-        System.out.println("** **   ** **   **   **  **     ");
-        System.out.println("** **   ** **   ******    ***** ");
-        System.out.println("** **   ** **   **            **");
-        System.out.println("** **   ** **   **      **   **");
-        System.out.println(" ***     ***    **       ***** ");
+        System.out.println(String.join(" ", " *** ", " *** ", "***** "," ***** "));
+        System.out.println(String.join(" ", "** **", "** **", "**  **", "**    "));
+        System.out.println(String.join(" ", "** **", "** **", "**  **", "**    "));
+        System.out.println(String.join(" ", "** **", "** **", "***** ", "***** "));
+        System.out.println(String.join(" ", "** **", "** **", "**    ", "    **"));
+        System.out.println(String.join(" ", "** **", "** **", "**    ", "    **"));
+        System.out.println(String.join(" ", " *** ", " *** ", "**    ", "***** "));
+
     }
 }
